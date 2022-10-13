c76143a3747851c1c5cec57c826ffb8085cf42f4
Nauman Shah <nshah@qualys.com>
2022-10-14 01:33:32 +0530

    learning file write and read completed

---
+++ 18files/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files in GoLang")
+	content := "This needs to go in a file - LearnCodeOnline.in"
+	fileName := "./myfile.txt"
+	file, err := os.Create(fileName)
+	checkNilErr(err)
+
+	length, err := io.WriteString(file, content)
+	fmt.Println("Length of the content is ", length)
+	defer file.Close()
+	ReadFile(fileName)
+}
+
+func ReadFile(filename string) {
+	dataByte, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+
+	fmt.Println("Text data inside the file is\n", string(dataByte))
+
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
